a0c4b28ba50520a538da878eb336a62153931b4d
qwerasdfxzdcv <104199005+qwerasdfxzdcv@users.noreply.github.com>
2024-09-05 19:41:33 +0900

    시장 상세정보 페이지에 아이콘 추가

---
+++ src/assets/img/icons/icons.js
@@ -0,0 +1,35 @@
+import loungeTrue from './loungeTrue.png';
+import loungeFalse from './loungeFalse.png';
+import lockerTrue from './lockerTrue.png';
+import lockerFalse from './lockerFalse.png';
+import bycicleFalse from './bycicleFalse.png'
+import bycicleTrue from './bycicleTrue.png'
+import helpdestFalse from './helpdeskFalse.png'
+import helpdestTrue from './helpdeskTrue.png'
+import mommyFalse from './mommyFalse.png'
+import mommyTrue from './mommyTrue.png'
+import parkingFalse from './parkingFalse.png'
+import parkingTrue from './parkingTrue.png'
+import playFalse from './playFalse.png'
+import playTrue from './playTrue.png'
+import voucherFalse from './voucherFalse.png'
+import voucherTrue from './voucherTrue.png'
+
+export {
+  loungeTrue,
+  loungeFalse,
+  lockerTrue,
+  lockerFalse,
+  bycicleFalse,
+  bycicleTrue,
+  helpdestFalse,
+  helpdestTrue,
+  mommyFalse,
+  mommyTrue,
+  parkingFalse,
+  parkingTrue,
+  playFalse,
+  playTrue,
+  voucherFalse,
+  voucherTrue
+}
